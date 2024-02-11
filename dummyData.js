582f1fd84476f9bed27275225c715e7c7ce38aff
KygoSkyrus <guptadheeraj371@gmail.com>
2024-02-12 00:04:12 +0530

    css changes

---
+++ dummyData.js
@@ -0,0 +1,48 @@
+const dummyMessages = [
+    {
+        "_id": "65c79a2a6f21248aeea46f90",
+        "name": "Aman kumar",
+        "email": "testuser@email.com",
+        "phone": "123456786",
+        "note": "Hi @skyBlog, really liked the blogs here.",
+        "__v": 0
+    }
+]
+
+const dummyUserSubmittedBlogs = [
+    {
+        "_id": "643d7b7aa3ac8d7bafaacfe4",
+        "email": "newtest@gmail.com",
+        "title": "10 things to know before buying your first bike.",
+        "detail": "<p style=\"text-align:justify;\"><span style=\"color: rgb(0,0,0);background-color: rgb(255,255,255);font-size: 14px;font-family: Open Sans;\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue dolor rhoncus ipsum hendrerit tempus. Aliquam dapibus imperdiet ante, eu rhoncus tellus ornare eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis odio turpis, consectetur sit amet posuere quis, volutpat ut diam. Donec blandit ex eros, eu porttitor nulla dictum sit amet. Aenean quis laoreet urna, ac consectetur sapien. Donec hendrerit consectetur interdum. Donec sit amet odio nisi. Sed vulputate, nunc a pulvinar congue, libero ex posuere libero, eu vestibulum massa augue vitae diam.</span></p>\n<p style=\"text-align:justify;\"><span style=\"color: rgb(0,0,0);background-color: rgb(255,255,255);font-size: 14px;font-family: Open Sans;\">Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse efficitur tincidunt volutpat. Nulla pretium mattis nulla interdum accumsan. Sed mattis arcu nec condimentum vulputate. Vivamus tristique justo scelerisque mattis accumsan. Donec condimentum dictum lorem ut condimentum. Sed ipsum elit, pulvinar quis sapien vel, mattis vulputate mauris. Morbi aliquam varius enim, eu feugiat nibh viverra in. Nunc ultrices mollis porta. Maecenas eu metus est. Maecenas et mi rutrum, euismod neque vel, sagittis ante. Etiam eget purus laoreet, tempus sem eu, elementum ante. Aenean et turpis ex. Vivamus tristique ipsum eu purus laoreet ornare.</span></p>\n<p style=\"text-align:justify;\"><span style=\"color: rgb(0,0,0);background-color: rgb(255,255,255);font-size: 14px;font-family: Open Sans;\">Cras eu imperdiet ligula. Quisque sapien quam, pulvinar id lacus vel, eleifend maximus augue. In sit amet leo luctus, aliquam orci eget, ornare lorem. Suspendisse porttitor eget quam mattis interdum. Curabitur quis massa ante. Nam vel ex erat. Quisque tincidunt vulputate lorem.</span></p>\n<p style=\"text-align:justify;\"><span style=\"color: rgb(0,0,0);background-color: rgb(255,255,255);font-size: 14px;font-family: Open Sans;\">Donec imperdiet neque nunc, vel gravida diam imperdiet in. Sed porta quam nisl, eget blandit mauris blandit eget. Aenean vehicula nisi quis elementum vulputate. Nullam facilisis purus pellentesque quam molestie tristique. Curabitur a tortor rhoncus, iaculis nunc at, lacinia nulla. Ut mi felis, vulputate a efficitur non, tincidunt quis nisi. Etiam in suscipit tellus. Nulla gravida nibh nec elit ultricies posuere. Nam at quam tincidunt, sodales est ullamcorper, consequat sem. Duis ut mauris dapibus leo lacinia feugiat.</span></p>\n<p style=\"text-align:justify;\"><span style=\"color: rgb(0,0,0);background-color: rgb(255,255,255);font-size: 14px;font-family: Open Sans;\">Mauris elementum porta sem, in aliquet erat porta id. Integer imperdiet placerat dolor, in ullamcorper enim sollicitudin vitae. Cras eu est euismod, tincidunt ipsum sed, pretium ante. Vestibulum hendrerit, quam a congue blandit, magna augue commodo augue, ut tincidunt odio est at mi. In turpis sem, posuere sed placerat vitae, sodales et neque. Vestibulum dui est, vehicula interdum aliquam eu, mollis auctor nulla. Aliquam pulvinar lorem odio, eget venenatis lorem sollicitudin eget. Morbi a quam non odio pretium feugiat quis sed neque. Maecenas id tellus at nulla interdum rhoncus vitae ac eros. Cras nisl mi, pretium eu tortor sit amet, malesuada dignissim sapien. Curabitur ac commodo nisi, vestibulum vulputate nulla. Suspendisse scelerisque lacinia justo eget porttitor. Sed sit amet sapien quis velit consectetur efficitur. Sed accumsan auctor vehicula. Morbi volutpat pellentesque vestibulum. Morbi malesuada cursus velit, vitae imperdiet enim finibus eget.</span>&nbsp;&nbsp;</p>\n",
+        "image": "https://firebasestorage.googleapis.com/v0/b/shopp-itt.appspot.com/o/skyblog%2F0bb9cc8c-548e-4966-af17-6696f0bcbc28?alt=media&token=015c0d7e-bf98-43b7-9489-f017a85d0097 ",
+        "category": "gyughh",
+        "type": "popular blogs",
+        "authorname": "Rahul dwivedi",
+        "shortdescription": "",
+        "metatitle": "",
+        "metakeywords": "",
+        "metadescription": "",
+        "date": "17/4/2023",
+        "__v": 0
+    },
+    {
+        "_id": "65c8ecaf1e58567cb86c6408",
+        "email": "dheerajgupta.whyshy@gmail.com",
+        "title": "Cheapest cities to live in spain",
+        "detail": "<p style=\"text-align:justify;\"><span style=\"color: rgb(0,0,0);background-color: rgb(255,255,255);font-size: 14px;font-family: Open Sans;\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue dolor rhoncus ipsum hendrerit tempus. Aliquam dapibus imperdiet ante, eu rhoncus tellus ornare eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis odio turpis, consectetur sit amet posuere quis, volutpat ut diam. Donec blandit ex eros, eu porttitor nulla dictum sit amet. Aenean quis laoreet urna, ac consectetur sapien. Donec hendrerit consectetur interdum. Donec sit amet odio nisi. Sed vulputate, nunc a pulvinar congue, libero ex posuere libero, eu vestibulum massa augue vitae diam.</span></p>\n<p style=\"text-align:justify;\"><span style=\"color: rgb(0,0,0);background-color: rgb(255,255,255);font-size: 14px;font-family: Open Sans;\">Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse efficitur tincidunt volutpat. Nulla pretium mattis nulla interdum accumsan. Sed mattis arcu nec condimentum vulputate. Vivamus tristique justo scelerisque mattis accumsan. Donec condimentum dictum lorem ut condimentum. Sed ipsum elit, pulvinar quis sapien vel, mattis vulputate mauris. Morbi aliquam varius enim, eu feugiat nibh viverra in. Nunc ultrices mollis porta. Maecenas eu metus est. Maecenas et mi rutrum, euismod neque vel, sagittis ante. Etiam eget purus laoreet, tempus sem eu, elementum ante. Aenean et turpis ex. Vivamus tristique ipsum eu purus laoreet ornare.</span></p>\n<p style=\"text-align:justify;\"><span style=\"color: rgb(0,0,0);background-color: rgb(255,255,255);font-size: 14px;font-family: Open Sans;\">Cras eu imperdiet ligula. Quisque sapien quam, pulvinar id lacus vel, eleifend maximus augue. In sit amet leo luctus, aliquam orci eget, ornare lorem. Suspendisse porttitor eget quam mattis interdum. Curabitur quis massa ante. Nam vel ex erat. Quisque tincidunt vulputate lorem.</span></p>\n<p style=\"text-align:justify;\"><span style=\"color: rgb(0,0,0);background-color: rgb(255,255,255);font-size: 14px;font-family: Open Sans;\">Donec imperdiet neque nunc, vel gravida diam imperdiet in. Sed porta quam nisl, eget blandit mauris blandit eget. Aenean vehicula nisi quis elementum vulputate. Nullam facilisis purus pellentesque quam molestie tristique. Curabitur a tortor rhoncus, iaculis nunc at, lacinia nulla. Ut mi felis, vulputate a efficitur non, tincidunt quis nisi. Etiam in suscipit tellus. Nulla gravida nibh nec elit ultricies posuere. Nam at quam tincidunt, sodales est ullamcorper, consequat sem. Duis ut mauris dapibus leo lacinia feugiat.</span></p>\n<p style=\"text-align:justify;\"><span style=\"color: rgb(0,0,0);background-color: rgb(255,255,255);font-size: 14px;font-family: Open Sans;\">Mauris elementum porta sem, in aliquet erat porta id. Integer imperdiet placerat dolor, in ullamcorper enim sollicitudin vitae. Cras eu est euismod, tincidunt ipsum sed, pretium ante. Vestibulum hendrerit, quam a congue blandit, magna augue commodo augue, ut tincidunt odio est at mi. In turpis sem, posuere sed placerat vitae, sodales et neque. Vestibulum dui est, vehicula interdum aliquam eu, mollis auctor nulla. Aliquam pulvinar lorem odio, eget venenatis lorem sollicitudin eget. Morbi a quam non odio pretium feugiat quis sed neque. Maecenas id tellus at nulla interdum rhoncus vitae ac eros. Cras nisl mi, pretium eu tortor sit amet, malesuada dignissim sapien. Curabitur ac commodo nisi, vestibulum vulputate nulla. Suspendisse scelerisque lacinia justo eget porttitor. Sed sit amet sapien quis velit consectetur efficitur. Sed accumsan auctor vehicula. Morbi volutpat pellentesque vestibulum. Morbi malesuada cursus velit, vitae imperdiet enim finibus eget.</span>&nbsp;&nbsp;</p>\n",
+        "url": "cheapest-cities-to-live-in-spain",
+        "image": "https://firebasestorage.googleapis.com/v0/b/shopp-itt.appspot.com/o/skyblog%2Fpexels-yan-krukau-5792901.jpg?alt=media&token=bc662b81-5247-4f2f-839e-a5ec6ea3874b",
+        "category": "lifestyle",
+        "type": "featured blogs",
+        "authorname": "Tushar Kumar",
+        "shortdescription": "Explore the unexplored wonders of Europe",
+        "metatitle": "",
+        "metakeywords": "",
+        "metadescription": "",
+        "date": "2/11/2024",
+        "__v": 0
+    },
+]
+
+module.exports = {dummyMessages,dummyUserSubmittedBlogs}
